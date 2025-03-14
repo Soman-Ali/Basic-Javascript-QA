e0ef3be4bdede427798e25d97514ffb9382aca56
Soman-Ali <info.soman83@gmail.com>
2025-03-14 23:15:50 +0530

    do some basic question of javascript let var const function hoisting clousers

--- js/let-var-const.js
+++ js/let-var-const.js
@@ -13,7 +13,7 @@
 // console.log(typeof test)
 // console.log(test());
 
-// answer is undefined
+// answer is undefined because test is not defined and then test is defined and then test is called so 5 is printed
 // ------------------------------------------------------------
 // --------------------------------------------------------------
 
@@ -27,7 +27,7 @@
 // }
 // scopeTest();
 
-// answr is undefined
+// answr is undefined because x is defined after console.log(x) so x is undefined
 // ------------------------------------------------------------
 // --------------------------------------------------------------
 
@@ -44,7 +44,7 @@
 // let increment = createIncrementer();
 // console.log(increment());
 // console.log(increment());
-// answer is 1 and 2
+// answer is 1 and 2 because count is 0 and increment is called so count is 1 and again increment is called so count is 2
 
 // ===================================================================
 
@@ -54,7 +54,7 @@
 // console.log(foo);
 // var foo = 10;
 
-// ans is undefined
+// ans is undefined because foo is not defined
 
 // ==========================================================================
 
@@ -70,7 +70,7 @@
 //   console.log(x);
 // }
 // blockScope();
-// ans is 20 10
+// ans is 20 10 because x is 20 in if block and x is 10 outside if block
 
 
 // ================================================================================
@@ -86,7 +86,7 @@
 // var innerFunc = outer();
 // innerFunc();
 
-// ans is outer
+// ans is outer because outerVar is defined in outer function
 // ================================================================================
 // 7. Variable Shadowing
 
@@ -99,7 +99,7 @@
 // }
 // shadow();
 // console.log(a);
-// ans is 5 10
+// ans is 5 10 because a is 5 in function and a is 10 outside function
 
 // ================================================================================
 
@@ -124,22 +124,163 @@
 // counter.increment();
 // counter.decrement();
 
-// ans is 1 nd 0
+// ans is 1 nd 0 because count is 0 and increment is called so count is 1 and decrement is called so count is 0  again 
 
 // ================================================================================
 // 9. Lexical Scoping with Functions
 
-function outerFunc(){
-    let x = 10;
-    function innerFunc(){
-        console.log(x);
-    }
-    return innerFunc;
-}
-let result = outerFunc();
-result();
+// function outerFunc(){
+//     let x = 10;
+//     function innerFunc(){
+//         console.log(x);
+//     }
+//     return innerFunc;
+// }
+// let result = outerFunc();
+// result();
 
-git 
+// ans is 10 because x is defined in outer function
+
+// ================================================================================
+
+// 10. Hoisting with Function Declarations
+
+// function hoistingTest(){
+//     console.log(x);
+//     var x = 10;
+// }
+// hoistingTest();
+// ans is undefined because x is not defined
+// ================================================================================
+
+// 11. Functions and Default Parameters
+
+// function add(a=5,b=a*2){
+//     return a+b;
+// }
+// console.log(add(3));
+// console.log(add());
+// ans is 8 and 15 because 3+6=9 and 5+10=15 
+
+// ================================================================================
+// 12. Function Hoisting with Function Expressions
+
+// console.log(test());
+// var test = function() { return 5; };
+// ans is error because test is not defined
+// ================================================================================
+// 13. Function Scope with Var
+
+// function scopeTest() {
+//     if (true) { 
+//         var x = 10;
+//     }
+//     console.log(x);
+// }
+// scopeTest();
+// ans is 10 because x is defined in if block
+// ================================================================================
+
+// 12. Closure with State
+// function createCounter(){
+//     let count =0;
+//     return function(){
+//         return ++count;
+//     };
+// }
+// const counter = createCounter();
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+// ans is 1 2 3 because count is 0 and increment is called so count is 1 and again increment is called so count is 2 and again increment is called so count is 3
+// ================================================================================
+
+// 13. Hoisting in Functions
+
+// function testHoist(){
+//     console.log(foo);
+//     var foo = 10;
+// }
+// testHoist();
+// ans is undefined because foo is not defined
+
+// ================================================================================
+
+// 14. Self-Invoking Function with Closure
+
+// (function(){
+//     var x=5;
+// })();
+// console.log(x);
+// ans is error because x is not defined
+
+// ================================================================================
+
+// 15. Closure and Returning Functions
+
+// function multiplierFactory(factor){
+//     return function(num){
+//         return num*factor;
+//     };
+// }
+// let double = multiplierFactory(2);
+// console.log(double(4));
+// ans is 8 because 4*2=8 because double is 2 and 4 is passed to double so 4*2=8 and printed 8 
+// ================================================================================
 
 
+// 16. Block Scope and Let
+
+// let x=5;
+// function checkScope(){
+//     let x=10;
+//     console.log(x);
+// }
+// checkScope();
+// console.log(x);
+// ans is 10 5 because x is 10 in function and x is 5 outside function
+// ================================================================================
+
+// 17. Functions and Parameter Hoisting
+
+// function hoistParam(param){
+//     console.log(param);
+//     var param = 10;
+// }
+// hoistParam(5);
+// ans is undefined because param is not defined
+// ================================================================================
+// 18. Nested Functions and Scope Chain
+
+// let x=1;
+// function outer(){
+//     let x=2;
+//     function inner(){
+//         console.log(x);
+//     }
+//     inner();
+// }
+// outer();
+// ans is 2 because x is 2 in inner function
+
+// ================================================================================
+
+// 19. Arguments Object
+
+// function testArgs(a,b,c){
+//     console.log(arguments[0],arguments[1],arguments[2]);
+// }
+// testArgs(1,2);
+// ans is 1 2 undefined because 1 and 2 are passed to a and b and c is not passed so c is undefined
+
+// ================================================================================
+// 20. Variable Shadowing in Blocks
+
+// let x=5;
+// {
+//     let x=10;
+//     console.log(x);
+// }
+// console.log(x);
+// ans is 10 5 because x is 10 in block and x is 5 outside block
 
